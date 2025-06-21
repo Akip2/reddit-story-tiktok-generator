85d02392de7db45842d0e36ab4f1a3fba2ae402c
Antoine FONTANEZ <126561427+Akip2@users.noreply.github.com>
2025-06-22 01:24:27 +0200

    Generating srt subtitles file

--- video_editor.py
+++ video_editor.py
@@ -1,8 +1,11 @@
-import ffmpeg, os, random
+import ffmpeg, os, random, whisper
 from datetime import datetime
 from mutagen.mp3 import MP3
+from whisper.utils import get_writer
 
 GAMEPLAY_DIR = "./gameplay"
+
+model = whisper.load_model("base")
 
 def get_random_gameplay():
     return os.path.join(GAMEPLAY_DIR, random.choice(os.listdir(GAMEPLAY_DIR)))
@@ -14,6 +17,11 @@ def get_audio_duration(audio_path: str) -> float:
 def get_video_duration(video_path: str) -> float:
     probe = ffmpeg.probe(video_path)
     return float(probe["format"]["duration"])
+
+def generate_subtitles(audio_path: str):
+    transcription = model.transcribe(audio_path, fp16=False)
+    sub_writer = get_writer("srt", "./output")
+    sub_writer(transcription, "subtitles.srt")
 
 def generate_video_from_audio(audio_path: str):
     video_path = get_random_gameplay()
